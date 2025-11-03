45ce0e994ba8cf7a237964b2afd10a6b5414b82f
pablosanchez-pp <pablo.sanchez@petroprix.com>
2025-11-03 11:14:10 +0100

    feat: [SECURITY]: Añadir interceptor de validación de edad por JWT en merchant-microservice

---
+++ merchant-microservice/src/main/java/com/tareasPracticas/merchant_microservice/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.tareasPracticas.merchant_microservice.config;
+
+import com.tareasPracticas.merchant_microservice.config.interceptor.AgeInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+    private final AgeInterceptor ageInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(ageInterceptor)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns(
+                        "/swagger-ui/**",
+                        "/v2/api-docs",
+                        "/v3/api-docs/**",
+                        "/actuator/**"
+                );
+    }
+}
